31c91272c5edd9c356a53e4d15f69582ecbd089f
christiandigiandomenico <christian.digiandomenico1990@gmail.com>
2024-04-09 12:20:52 +0200

    Data printed

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,5 +3,8 @@ import '~resources/scss/app.scss';
 import.meta.glob([
     '../img/**'
 ]);
-import '~resources/scss/app.scss';
-import * as bootstrap from 'bootstrap';
+/* Import Bootstrap 5 */
+// Import our custom CSS
+import '~resources/scss/app.scss'
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
