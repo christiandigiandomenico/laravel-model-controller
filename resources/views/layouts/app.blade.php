b476527f227b88e7b78f546ee8dc402625c70c1d
christiandigiandomenico <christian.digiandomenico1990@gmail.com>
2024-04-09 17:12:59 +0200

    Added some style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,23 @@
     @vite('resources/js/app.js')
 </head>
 <body>
+
+    <div class="title-container">
+        <h1>Movie Database</h1>
+    </div>
     
+    <div class="container">
+    @foreach($movies as $movie)
+    <div class="card cardItem" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">{{$movie->title}}</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary nationality-name">{{$movie->nationality}}</h6>
+          <h6 class="card-subtitle mb-2 text-body-secondary">{{$movie->date}}</h6>
+          <p class="card-text">{{$movie->vote}}</p>
+        </div>
+    </div>
+    @endforeach
+    </div>
 
 </body>
 </html>
